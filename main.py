44eeadca19d0cd20940d95fb267387a47b63e84c
Sarah Klein <8sarahklein8@gmail.com>
2017-01-30 11:44:33 -0500

    adding libraries

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import os
 import jinja2
 import webapp2
 import logging
+import json
+import urllib
+
+# this is used for constructing URLs to google's APIS
+from googleapiclient.discovery import build
+
+# This uses discovery to create an object that can talk to the 
+# fusion tables API using the developer key
+service = build('fusiontables', 'v1', developerKey=API_KEY)
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
